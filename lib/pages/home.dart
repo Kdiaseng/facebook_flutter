dc189f12f363055609ab27a13525d943fdeffd49
Kdiaseng <kalebdias.oficial@gmail.com>
2021-11-18 22:37:55 -0400

    feat: init implemented responsive layout mobile and desktop

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -4,6 +4,7 @@ import 'package:facebook_flutter/components/button_circle.dart';
 import 'package:facebook_flutter/components/card_post.dart';
 import 'package:facebook_flutter/data/dados.dart';
 import 'package:facebook_flutter/utils/pallet_colors.dart';
+import 'package:facebook_flutter/utils/responsive.dart';
 import 'package:flutter/material.dart';
 import 'package:line_icons/line_icons.dart';
 
@@ -17,60 +18,108 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            backgroundColor: Colors.white,
-            // expandedHeight: 250,
-            floating: true,
-            centerTitle: false,
-            title: const Text(
-              'facebook',
-              style: TextStyle(
-                  color: PalletColors.azulFaceFacebook,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 28,
-                  letterSpacing: -1.2),
-            ),
-            actions: [
-              ButtonCircle(
-                icon: Icons.search,
-                iconSize: 30,
-                onPressed: () {},
-              ),
-              ButtonCircle(
-                icon: LineIcons.facebookMessenger,
-                iconSize: 30,
-                onPressed: () {},
-              )
-            ],
-          ),
-          SliverToBoxAdapter(
-            child: AreaPosts(
-              user: userCurrent,
-            ),
-          ),
-          SliverPadding(
-            padding: const EdgeInsets.fromLTRB(0, 10, 0, 5),
-            sliver: SliverToBoxAdapter(
-              child: AreaStory(
-                user: userCurrent,
-                stories: stories,
-              ),
-            ),
-          ),
-          SliverList(
-            delegate: SliverChildBuilderDelegate(
-              (context, index) {
-                final post = posts[index];
-                return CardPosts(post: post);
-              },
-              childCount: posts.length,
-            ),
-          ),
-        ],
+    return const Scaffold(
+      body: Responsive(
+        mobile: HomeMobile(),
+        desktop: HomeDesktop(),
       ),
+    );
+  }
+}
+
+class HomeMobile extends StatelessWidget {
+  const HomeMobile({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          backgroundColor: Colors.white,
+          // expandedHeight: 250,
+          floating: true,
+          centerTitle: false,
+          title: const Text(
+            'facebook - M',
+            style: TextStyle(
+                color: PalletColors.azulFaceFacebook,
+                fontWeight: FontWeight.bold,
+                fontSize: 28,
+                letterSpacing: -1.2),
+          ),
+          actions: [
+            ButtonCircle(
+              icon: Icons.search,
+              iconSize: 30,
+              onPressed: () {},
+            ),
+            ButtonCircle(
+              icon: LineIcons.facebookMessenger,
+              iconSize: 30,
+              onPressed: () {},
+            )
+          ],
+        ),
+        SliverToBoxAdapter(
+          child: AreaPosts(
+            user: userCurrent,
+          ),
+        ),
+        SliverPadding(
+          padding: const EdgeInsets.fromLTRB(0, 10, 0, 5),
+          sliver: SliverToBoxAdapter(
+            child: AreaStory(
+              user: userCurrent,
+              stories: stories,
+            ),
+          ),
+        ),
+        SliverList(
+          delegate: SliverChildBuilderDelegate(
+            (context, index) {
+              final post = posts[index];
+              return CardPosts(post: post);
+            },
+            childCount: posts.length,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class HomeDesktop extends StatelessWidget {
+  const HomeDesktop({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      slivers: [
+
+        SliverPadding(
+          padding: const EdgeInsets.fromLTRB(0, 10, 0, 5),
+          sliver: SliverToBoxAdapter(
+            child: AreaStory(
+              user: userCurrent,
+              stories: stories,
+            ),
+          ),
+        ),
+        SliverToBoxAdapter(
+          child: AreaPosts(
+            user: userCurrent,
+          ),
+        ),
+        SliverList(
+          delegate: SliverChildBuilderDelegate(
+            (context, index) {
+              final post = posts[index];
+              return CardPosts(post: post);
+            },
+            childCount: posts.length,
+          ),
+        ),
+      ],
     );
   }
 }
--- lib/utils/responsive.dart
+++ lib/utils/responsive.dart
@@ -1,22 +1,38 @@
 import 'package:flutter/material.dart';
 
 class Responsive extends StatelessWidget {
-  const Responsive({Key? key}) : super(key: key);
+  final Widget mobile;
+  final Widget desktop;
 
-  static bool isMobile(BuildContext context){
+  const Responsive({
+    Key? key,
+    required this.mobile,
+    required this.desktop,
+  }) : super(key: key);
+
+  static bool isMobile(BuildContext context) {
     return MediaQuery.of(context).size.width < 800;
   }
 
-  static bool isTablet(BuildContext context){
-    return MediaQuery.of(context).size.width >= 800 && MediaQuery.of(context).size.width < 1200 ;
+  static bool isTablet(BuildContext context) {
+    return MediaQuery.of(context).size.width >= 800 &&
+        MediaQuery.of(context).size.width < 1200;
   }
 
-  static bool isDesktop(BuildContext context){
+  static bool isDesktop(BuildContext context) {
     return MediaQuery.of(context).size.width > 1200;
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth > 1200) {
+          return desktop;
+        }
+
+        return mobile;
+      },
+    );
   }
 }
